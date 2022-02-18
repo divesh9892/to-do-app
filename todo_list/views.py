9e8005a6907661cafb9b6f8ecd3e971a116ea763
Divesh Sharma <45972450+divesh9892@users.noreply.github.com>
2022-02-18 11:20:36 +0530

    First Commit

---
+++ todo_list/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+
+
+def home(request):
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+        if form.is_valid():  # optional in newer version
+            form.save()
+            messages.success(request, 'Item Has Been Added To The List!')
+            all_items = List.objects.all
+            return render(request, 'home.html', {'all_items': all_items})
+    else:
+        all_items = List.objects.all
+        return render(request, 'home.html',{'all_items': all_items})
+
+
+def about(request):
+    context = {'firstname': 'Divesh', 'lastname': 'Sharma'}
+    return render(request, 'about.html', context)
+
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, 'Item Has Been Deleted')
+    return redirect('home')
+
+
+def cross_off(request, list_id):        # request is compulsory in argument
+    item = List.objects.get(pk=list_id)
+    item.completed = True
+    item.save()
+    return redirect('home')
+
+
+def uncross(request, list_id):           # request is compulsory in argument and should be passes as parameter or error
+    item = List.objects.get(pk=list_id)
+    item.completed = False
+    item.save()
+    return redirect('home')
+
+
+def edit(request, list_id):
+    if request.method == 'POST':
+        item = List.objects.get(pk=list_id)
+        form = ListForm(request.POST or None, instance=item)
+        if form.is_valid():  # optional in newer version
+            form.save()
+            messages.success(request, 'Item Has Been Edited')
+            return redirect('home')
+    else:
+        item = List.objects.get(pk=list_id)
+        return render(request, 'edit.html',{'item': item})
